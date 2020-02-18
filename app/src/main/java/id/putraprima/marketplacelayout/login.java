25a25e3247e4e2afcc66a3c238cd9e85fbdc1c7a
Fikky Miftakhul Huda <vqfikky93@gamil.com>
2020-02-18 08:50:48 +0700

    Konversi Layout 2

--- app/src/main/java/id/putraprima/marketplacelayout/login.java
+++ app/src/main/java/id/putraprima/marketplacelayout/login.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
     }
 }
